c1618e318429ba56ef363635ecb109335cfc4e32
ShashiKant <shashikant@paralleldots.com>
2023-09-16 15:00:32 +0530

    Adding segment index pages and its cache in simple hashmap Eviction policies not implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::error::Error;
 use std::fmt;
 use std::fs::{File, OpenOptions};
@@ -166,50 +167,126 @@ struct SegmentIndexPage {
     start_offset: i32,
     segment_id: i32,
     max_number_of_records: i32,
-    records: Vec<SegmentOffset>
+    bytes: Vec<u8>
 }
 
 impl SegmentIndexPage {
     fn new(max_number_of_records: i32, start_offset: i32, segment_id: i32) -> Self {
+        let size = SegmentOffset::size_of_single_record();
         return Self{
             max_number_of_records: max_number_of_records,
             start_offset: start_offset,
-            records: vec![],
+            bytes: vec![0u8; max_number_of_records as usize * size as usize],
             segment_id: segment_id
         }
     }
 
-    fn from_bytes(max_number_of_records: i32, start_offset: i32, segment_id: i32, bytes: &[u8]) -> Self {
+    fn from_bytes(max_number_of_records: i32, start_offset: i32, segment_id: i32, bytes: Vec<u8>) -> Self {
         let size = SegmentOffset::size_of_single_record() as usize;
-        let mut records = vec![];
-        for i in 0..max_number_of_records {
-            let offset_opt = SegmentOffset::from_bytes(
-                &bytes[size*(i as usize)..size*((i+1) as usize)]
-            );
-            if let Some(offset) = offset_opt {
-                records.push(offset)
-            } else {
-                //No more records in the page.
-                break
-            }
-        }
         return Self {
             max_number_of_records: max_number_of_records,
             segment_id: segment_id,
             start_offset: start_offset,
-            records: records
+            bytes: bytes
         }
     }
 
-    fn to_bytes(&self) -> Vec<u8> {
-        let mut bytes = vec![0u8; (self.max_number_of_records * SegmentOffset::size_of_single_record()) as usize];
-        let mut i = 0;
-        let size = SegmentOffset::size_of_single_record() as usize;
-        for sgoffst in &self.records {
-            bytes[i*size..(i+1)*size].copy_from_slice(&sgoffst.to_bytes());
-            i += 1;
+    fn add_segment_offset(&mut self, offset: &SegmentOffset) -> Option<()> {
+        let size = SegmentOffset::size_of_single_record();
+        let mut lo = 0;
+        let mut hi = self.max_number_of_records - 1;
+        let length = ((hi+1)*size) as usize;
+        let last_message_offset_res = SegmentOffset::from_bytes(
+            &self.bytes[length-size as usize..]
+        );
+        if last_message_offset_res.is_some() {
+            return None
+        }
+        let mut idx : Option<i32> = None;
+        while lo <= hi {
+            let mid = (lo+hi)/2;
+            let start_offset = (mid*size) as usize;
+            let msg_offset_opt = SegmentOffset::from_bytes(
+                &self.bytes[start_offset..start_offset+size as usize]
+            );
+            let pre_msg_offset_opt;
+            if lo != mid {
+                pre_msg_offset_opt= SegmentOffset::from_bytes(
+                    &self.bytes[start_offset-size as usize..start_offset]
+                );
+            } else {
+                if msg_offset_opt.as_ref().is_none() {
+                    idx = Some(mid);
+                    break;
+                }
+                pre_msg_offset_opt = None;
+            }
+            if pre_msg_offset_opt.as_ref().is_none() && msg_offset_opt.as_ref().is_none() {
+                if mid-1 == 0{
+                    idx = Some(0);
+                    break;
+                } else {
+                    hi = mid - 1;
+                }
+            } else if msg_offset_opt.as_ref().is_none() {
+                if pre_msg_offset_opt.as_ref().is_some_and(
+                    |x| x.message_offset < offset.message_offset
+                ) {
+                    idx = Some(mid);
+                    break;
+                } else {
+                    hi = mid - 1;
+                }
+            } else {
+                let mid_val = msg_offset_opt.as_ref().unwrap().message_offset;
+                let premid_val = msg_offset_opt.as_ref().unwrap().message_offset;
+                if offset.message_offset < mid_val && offset.message_offset > premid_val {
+                    idx = Some(mid);
+                    break
+                } else if offset.message_offset < mid_val && offset.message_offset < premid_val{
+                    lo = mid + 1;
+                } else {
+                    hi = mid - 1;
+                }
+            }
         };
-        return bytes;
+        let i = idx.unwrap();
+        let write_offset = i * size;
+        let after_bytes = &self.bytes[write_offset as usize..length-size as usize];
+        let pre_bytes = &self.bytes[0..write_offset as usize];
+        let mut bytes = vec![];
+        bytes.extend_from_slice(pre_bytes);
+        bytes.extend_from_slice(&offset.to_bytes());
+        bytes.extend_from_slice(&after_bytes);
+        self.bytes = bytes;
+        return Some(());
+    }
+
+    fn get_segment_offset(
+        &self,
+        message_offset: i64
+    ) -> Option<SegmentOffset> {
+        let size = SegmentOffset::size_of_single_record();
+        let mut lo = 0;
+        let mut hi = self.max_number_of_records - 1;
+        while lo <= hi {
+            let mid = (lo + hi) /2;
+            let mut offset = SegmentOffset::from_bytes(
+                &self.bytes[(mid as usize)*(size as usize)..(mid as usize + 1)*(size as usize)]
+            );
+            if offset.as_ref().is_some_and(
+                |x| x.message_offset == message_offset
+            ) {
+                return offset;
+            } else if offset.as_ref().is_none() || offset.as_ref().is_some_and(
+                |x| x.message_offset > message_offset
+            ) {
+                hi = mid - 1;
+            } else {
+                lo = mid + 1;
+            }
+        }
+        return None;
     }
 }
 
@@ -255,12 +332,105 @@ impl SegmentIndex {
             number_of_bytes: number_of_bytes
         }
     }
+
+    fn get_page(&self, start_offset: i32, number_of_records: i32) -> Result<SegmentIndexPage, MessageIOError> {
+        let size = SegmentOffset::size_of_single_record();
+        let number_of_bytes = number_of_records * size;
+        let mut buf = vec![0u8; number_of_bytes as usize];
+        let file = OpenOptions::new()
+            .read(true)
+            .write(false)
+            .open(String::from(&self.file))?;
+        file.seek_read(&mut buf, start_offset as u64)?;
+        return Ok(SegmentIndexPage {
+            start_offset: start_offset,
+            segment_id: self.segment_id,
+            max_number_of_records: number_of_records,
+            bytes: buf
+        })
+    }
+
+    fn write_page(&mut self, page: &SegmentIndexPage) -> Result<(), MessageIOError> {
+        if !self.active {
+            return Err(MessageIOError::SegmentOverFlow(String::from("This segment has already been closed for writing.")));
+        }
+        self.write_handler.as_ref().unwrap().seek_write(&page.bytes, page.start_offset as u64)?;
+        self.write_handler.as_ref().unwrap().flush()?;
+        return Ok(())
+    }
+}
+
+/*
+A simple index page cache implementation which uses only hashmap. Later more great cache with eviction policies may be implemented.
+*/
+struct SegmentIndexPageCache {
+    store: HashMap<i32, HashMap<i32, SegmentIndexPage>>,
+    max_pages_to_store: i32,
+    current_number_of_pages: i32
+}
+
+impl SegmentIndexPageCache {
+    fn new(max_pages_to_store: i32) -> Self {
+        return Self {
+            store: HashMap::new(),
+            max_pages_to_store: max_pages_to_store,
+            current_number_of_pages: 0
+        }
+    }
+
+    fn evict(&mut self) {
+        //implement later.
+    }
+
+    fn get(&mut self, segment_id: i32, page_offset: i32) -> Option<&mut SegmentIndexPage> {
+        match self.store.get_mut(&segment_id) {
+            Some(h) => match h.get_mut(&page_offset) {
+                Some(v) => Some(v),
+                None => None
+            },
+            None => None
+        }
+    }
+
+    fn set(&mut self, page: SegmentIndexPage) -> Option<SegmentIndexPage> {
+        match self.store.get_mut(&page.segment_id) {
+            Some(v) => {
+                match v.insert(page.start_offset, page) {
+                    Some(val) => {
+                        self.current_number_of_pages += 1;
+                        return Some(val);
+                    },
+                    None => {
+                        return None
+                    }
+                }
+            }
+            None => {
+                self.current_number_of_pages += 1;
+                let segment_id = page.segment_id;
+                let mut h = HashMap::new();
+                h.insert(page.start_offset, page);
+                self.store.insert(segment_id, h);
+                return None;
+            }
+        }
+    }
+
+    fn get_pages_of_segment(&mut self, segment_id: i32) -> Vec<&mut SegmentIndexPage> {
+        match self.store.get_mut(&segment_id) {
+            Some(h) => {
+                return h.values_mut().into_iter().collect();
+            },
+            None => {
+                return vec![];
+            }
+        }
+    }
 }
 
 #[cfg(test)]
 mod SegmentTests {
-    use crate::{Message, Segment};
-
+    use crate::{Message, Segment, SegmentIndexPage, SegmentIndexPageCache};
     #[test]
     fn test_segment_functions() {
         let mut segment = Segment::new(8196, 1, String::from("."), true);
@@ -279,6 +449,36 @@ mod SegmentTests {
             println!("Failed in writing {:?}", res);
             assert!(res.is_ok());
         }
+    }
+
+    #[test]
+    fn test_segment_index_page() {
+        let mut page = SegmentIndexPage::new(100, 0, 1);
+        let segment = crate::SegmentOffset { message_offset: 1, physical_offset: 1 };
+        page.add_segment_offset(&segment);
+        let searched_segment = page.get_segment_offset(1);
+        assert!(searched_segment.is_some_and(
+            |o| o.message_offset == segment.message_offset && o.physical_offset == segment.physical_offset
+        ))
+    }
+
+    #[test]
+    fn test_segment_index_page_cache() {
+        let mut cache = SegmentIndexPageCache::new(5);
+        let should_be_null = cache.get(1, 0);
+        assert!(should_be_null.is_none());
+        let again_null = cache.set(SegmentIndexPage::new(
+            5,
+            0,
+            1
+        ));
+        assert!(again_null.is_none());
+        let should_not_be_null = cache.get(1, 0);
+        assert!(should_not_be_null.is_some_and(
+            |x| {
+                x.segment_id == 1 && x.max_number_of_records == 5
+            }
+        ));
     }
 }
 
